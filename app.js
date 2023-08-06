0940723702a85e15913715d6e51b3367debb89c3
Sarthak Bose <135831439+Sarthak1402@users.noreply.github.com>
2023-08-06 08:22:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
     height: 1000,
-    width: 1200,
+    width: 1700,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
